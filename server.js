3eeb8204152befa63f72f56161b6bc008c5a7602
Leyang Pan <65788181+YuanSu526@users.noreply.github.com>
2024-01-01 20:38:55 -0800

    cannot setup avatar this way due to gcloud issue, need to look for another setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.use(express.json()); 
 
-const userRoutes = require('./_userRoutes').router;
+const userRoutes = require('./routes/_userRoutes').router;
 app.use(userRoutes);
 
 // Define a port to listen to
