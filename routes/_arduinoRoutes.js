9a6f772ae73e8a39351be0aa7f0bf98455983762
Leyang Pan <65788181+YuanSu526@users.noreply.github.com>
2024-01-22 20:39:57 -0800

    Arduino test success

--- routes/_arduinoRoutes.js
+++ routes/_arduinoRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // Middleware to parse JSON bodies
 router.use(express.json());
+router.use(express.text());
 
 const PORT = process.env.PORT || 8081;
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -22,16 +23,39 @@ module.exports = {
 
 router.post('/arduino/test', async (req, res) => {
     try {
-        const message = req.body;
+        let message;
+
+        if (req.is('application/json')) {
+            message = req.body;
+        } else if (req.is('text/plain')) {
+            message = req.body;
+        } else {
+            return res.status(400).send("Unsupported content type");
+        }
 
         console.log(message);
 
-        await mongoClient.db("BugRacket").collection("arduinoTest").insertOne(message);
+        await mongoClient.db("BugRacket").collection("arduinoTest").insertOne({ message: message });
 
-        res.status(200).send("Received Message: " + message);
+        if (typeof message === 'object') {
+            res.status(200).send("Received JSON Message: " + JSON.stringify(message));
+        } else {
+            res.status(200).send("Received Text Message: " + message);
+        }
 
     } catch (error) {
         console.log("Did not receive message");
         res.status(500).send("Did not receive message");
+    }
+});
+
+router.get("/arduino/test", async (req, res) => {
+    try{
+        const allMessages = await mongoClient.db("BugRacket").collection("arduinoTest").find({}).toArray();
+        res.send(allMessages);
+        console.log("All messages provided");
+    }catch(err){
+        res.status(500).send("Internal Server Error");
+        console.error("Internal Server Error");    
     }
 });
